c25c773d4df1e618f12ebcfed5839fd14cdd9e28
zzzworm <15828873+zzzworm86@user.noreply.gitee.com>
2025-08-17 11:00:39 +0800

    add jwtSecret for users-permissions

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,12 @@
-export default () => ({
+export default ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwt: {
+        expiresIn: "180d",
+      },
+      jwtSecret: env("JWT_SECRET"),
+  },
+},
 "strapi-plugin-populate-deep": {
     config: {
       defaultDepth: 3,
